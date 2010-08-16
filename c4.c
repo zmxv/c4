524db6da868a5deeff1f788e9f11e58d8aef50f4
Zhen Wang <3903349@gmail.com>
2010-08-15 17:19:45 -0700

    initial commit

---
+++ c4.c
@@ -0,0 +1,62 @@
+char                 c_,o_,n_,n ,e ,c ,t                [2006]
+                       ,*f,*o ,u, r ;
+
+                     unsigned char *b,y;
+                       #define w main
+          #define a(w)*o++=(c>40&&n>2&&n<10&&w)*17;
+
+
+   #     define    g(n,a   ,    w    )char    *n(){   return\
+   #          a         +  (    w    )        ;             }
+   #     define    n(a,w    )b+=a     *c+w    ; if(*        \
+   l     [              z       ]         (   )    )       \
+   {     return    -2;}/*       4     -in-    a-row      */
+
+
+g(A,FOO,2+!(*b&&((*b-1)&*b)==0))g(B,BAR,w(-4))g(C,BNF,(n+=(9-3
+*n)%5,!  f))g(D,  MPU,(o_  =-67,e=  -1,!r))  g(M,MD5  ,w(-357)
+&&c>o_)  g(I,QED  ,c>*(f+  39))g(G  ,HLT,f   >o)g(H,  JAR,(n_
+=o[*f]-  1,2+w(-  1)))g(N  ,ASM ,(  ++e<7)?  1+!o[*f  =e]:0)g(
+S,KIT,f  -t!=3)g  (K,FUN,  !(c=32+  (f-t)/   52))g(L  ,FSM,(c=
+r *u+!(  rand()%  4),!t))  g(W,WTF  ,(r=-r,  f+=52,w  (-2),c=-
+*(f-13)  ,2))g(O  ,NOP,++  *(f+38)  <7?1-!o  [*f=*(f  +38)]:2)
+g(P,                                                       GPL
+,(*                                                        (f+
+4)=-*(f  +60),*(  f+8)=-*  (f+56),  !r))g(Q  ,VAX,(*  (f+39)==
+-67)&&(  *(f+39)  =0))g(E  ,MAC,(n  =e,o_=c  ,!o))g(  T,GNU,!(
+*(f+4)=  -(*(f+8  )=67)))  g(F,QNX  ,(* (f+  39)=c,!  o)) g(R,
+UTF,(-*  (f+39))  <*(f+60  ))g (U,  BUG,(*(  f+60)=-  *(f+39),
+r&1))g(  V,VPN,*  (f+60)>  *(f+56)  ?2 :(*(  f+39)=-  *(f+60),
+!f ))g(  J,NAN,(  w(-3),f  -=52, *  (f+39)=  -67,r=-  r,!(*(f+
+38)                                                        =-1
+)))                                                        g(X
+,OBF,((  y=*b)&&  (*b= y+  (y&c_))  &&(u+=(  (24==*b  )-(129==
+*b ))),  3))g(Y,  USC,((y  =*b)&&(  *b=y-((  y&c_)>>  1))&& (u
+-=(24==  y)-(129  ==y)),3  )) g(Z,  ATE,2+!  (c_&*b)) char*(*l
+[])()={  A,B,C,D  ,E,F,G,  H,I,J,K  ,L,M,N,  O,P,Q,R  ,S,T,U,V
+,W,X,Y,  Z} ;int  w(int z) {char*e  ;switch  ( - z){  default:
+if(z<-5  )return  z >-8 ?  putchar  (!(z+6)  *22+10): (putchar
+(89                                                        -(z
+&63                                                        )),
+w(z>>6)  );for(r  =1,o=t,  c=u=0;c  <114;++  c){n=c%  13;a(n<7
+)a(c<80  &&n<7)a  (c<80)a  (c<80&&  n>5)}f=  (o+=4)+  11;for(c
+=7;c--;  o[c]=6)  ; *f=3;  for(w(-  3);;w(-  3)){for (c=8;--c;
+w(-351+  c));for  (w(-7);  c<42 &&  putchar( 1447759  >>(t [(c
+<<2)+(c  >38)*2]  *7+7));  )w (-!(  ++c%7)-  6);*f=n  ;w(0)&&w
+( r* 6-  374)&&w  (-99083  * 698);  for(c=7; c--||(w  (6669349
+* -                                                        15)
+&&w                                                        ( -
+451681*  174));)  if(o[c]  )break;  if(0<(r  = -r ))  {for( e=
+"C";*e-  65;)e=l  [*e-65]  (); w(-  20952-n  )&& w(-  25569- (
+o_>31)*  1728-(-  38>o_ )  * 2688)  ; }else  for(w(-  98811264
+)&& w(-  97750779 ); ( n=  getchar  ())==48  &&w(-5)  ||n<48||
+n>55||!  o[n-=49  ];);*f=  n;}case  5:exit(  0);case  4:for(c=
+r-1;++c  <r+26;l  [c%26]=  l[( c+1  )% 26])  ;return  r;case 3
+: c                                                        = (
+c_=                                                        n_=
+--o[* f  ])*7 +*  f ;t[(c  << 2) +  (c >38)  * 2] =r  ;break ;
+case 2:  c= (c_=  n_ =o[*  f] ++)*  7+* f;t  [(c <<2  )+ (38<c
+)*2] =0  ; break  ;case 1  :case 0  : c_=n_  ; break  ; }e = t
++ *f*4+  c_ *52+  168 ;c_  = (112*  (z < -1  )+ 128)  >> 4* (r
+> 0); z  =(z+ *(  l[ 10 ]  ( )+ 2)  )% 26 ;  for(c =  4; --c&&
+(b = e);){ n(1,0)n(-5,0)n(-52,1)n(4,1)n(4,1)}n(0,45)return 0;}
